76973fa5bbc1fca24363223fc89e52869c6c5601
development <pmsfriend@googlemail.com>
2018-06-03 18:43:26 +0200

    Working version for PI

--- EntryPoint_DBServer/docker-entrypoint.sh
+++ EntryPoint_DBServer/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-echo "Starting Services"
+echo "********* Welcome to start script of DocBox on Docker ********"
+echo "Starting Avahi Services and DBus Mapping"
 rm //var/run/avahi-daemon/pid || true
 /etc/init.d/dbus start
 until [ -e /var/run/dbus/system_bus_socket ]; do
@@ -7,12 +8,30 @@ until [ -e /var/run/dbus/system_bus_socket ]; do
     echo "dbus-daemon is not running on hosting server..."
     sleep 1s
 done
+
+echo "Checking if Database is set-up..takes some time"
+#https://github.com/vishnubob/wait-for-it
+//init/wait_for_it.sh mysql:3306 --timeout=0
+cd DBServer
+if rake db:migrate:status>/dev/null 2>&1 ; then
+    echo "***** Database exists - continue booting"
+else
+    echo "****** Assuming fresh installation, creating DB and Index *****"
+    rake db:create
+    rake db:schema:load
+    rake db:seed
+    rake ts:configure
+    rake assets:precompile
+fi
+
+
+echo "Start NGINX"
 /etc/init.d/nginx start
 #/etc/init.d/avahi-daemon start
+echo "Start Avahi Daemon"
 exec avahi-daemon --no-chroot &
-cd DBServer
-echo "Starting Index Service"
-rake ts:start
+echo "Sphinx Index"
+rake ts:index
 echo "Starting DBServer"
 god start docbox -c docbox.god.rb -D
 echo "Starting Service: DONE"
--- EntryPoint_DBServer/docker_init_server.sh
+++ EntryPoint_DBServer/docker_init_server.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+cd DBServer
 rake db:create
 rake db:schema:load
 rake db:seed
 rake ts:configure
 rake ts:index
-rake ts:start
 rake assets:precompile
-
