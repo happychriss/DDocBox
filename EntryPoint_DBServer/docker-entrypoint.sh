b1747b984790dbb1054a9265a91a1651eb5f5a44
development <pmsfriend@googlemail.com>
2018-06-08 23:52:49 +0200

    Working version for PI

--- EntryPoint_DBServer/docker-entrypoint.sh
+++ EntryPoint_DBServer/docker-entrypoint.sh
@@ -22,7 +22,7 @@ else
     rake db:seed
     rake ts:configure
     rake assets:precompile
-    echo "****** Finished initial Installation *****"
+    echo "****** Finished initial Installations *****"
 fi
 
 
